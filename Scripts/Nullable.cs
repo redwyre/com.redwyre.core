1757795c0b98a724ff4b4409bf285d483add6c2a
Simon <redwyre@users.noreply.github.com>
2026-01-01 14:08:55 +1000

    add nullable helpers

---
+++ Scripts/Nullable.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+#nullable enable
+
+public class Nullable
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsNullOrEmpty([NotNullWhen(false)] string? value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] string? value)
+    {
+        return string.IsNullOrWhiteSpace(value);
+    }
+}
